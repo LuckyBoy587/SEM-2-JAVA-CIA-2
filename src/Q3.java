1b0a7a7975a3ee563127ec52e0c18e6701224882
LuckyBoy587 <kowshi587@gmail.com>
2024-06-13 11:27:32 +0530

    Answers

--- src/Q3.java
+++ src/Q3.java
@@ -73,9 +73,9 @@ public class Q3 {
     }
 
     public static void printMatrix(int[][] matrix) {
-        for (int i = 0; i < matrix.length; i++) {
+        for (int[] ints : matrix) {
             for (int j = 0; j < matrix[0].length; j++) {
-                System.out.print(matrix[i][j] + " ");
+                System.out.print(ints[j] + " ");
             }
             System.out.println();
         }
